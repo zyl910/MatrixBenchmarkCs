bf0d1de2854c091a9816a19e49aa0d3f9c1b16c8
zyl910 <zyl910@sina.com>
2026-07-29 01:37:39 +0800

    Move `TraitsINumberBase_Where.cs` to `HasWhere/TraitsINumberBase.cs`

--- MatrixLib.Impl/MathTraits/HasWhere/TraitsINumberBase.cs
+++ MatrixLib.Impl/MathTraits/HasWhere/TraitsINumberBase.cs
@@ -3,14 +3,14 @@ using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-namespace MatrixLib.MathTraits {
+namespace MatrixLib.MathTraits.HasWhere {
 
 #if NET7_0_OR_GREATER
     /// <summary>
     /// <see cref="INumberBase{TSelf}"/> 的类型萃取, 有约束.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public readonly struct TraitsINumberBase_Where<T>
+    public readonly struct TraitsINumberBase<T>
         where T : INumberBase<T>
 #if NET9_0_OR_GREATER
         //, allows ref struct // 这是因为 INumberBase 的 T 尚不支持 `allows ref struct` // CS9244 The type 'T' may not be a ref struct or a type parameter allowing ref structs in order to use it as parameter 'TSelf' in the generic type or method 'INumberBase<TSelf>'
