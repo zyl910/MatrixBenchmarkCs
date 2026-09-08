c85a8820f496bfed1812febce834ba0913db6516
zyl910 <zyl910@sina.com>
2026-09-09 01:16:07 +0800

    StructNumberBase using GenericMaths.

--- MatrixLib.Impl/MathTraits/CallerNumbers/StructNumberBase.Caller.cs
+++ MatrixLib.Impl/MathTraits/CallerNumbers/StructNumberBase.Caller.cs
@@ -8,6 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MatrixLib.MathTraits.CallerNumbers {
+#if NET7_0_OR_GREATER
+    using TraitsNS = MatrixLib.MathTraits.GenericMaths;
+#else
+    using TraitsNS = MatrixLib.MathTraits;
+#endif // NET7_0_OR_GREATER
+
     partial struct StructNumberBase<T> : INumberBaseCaller<StructNumberBase<T>>, IBaseMathCallerRegister {
 
         /// <inheritdoc cref="IBaseMathCallerRegisterDocument.CallerRegister"/>
@@ -21,12 +27,20 @@ namespace MatrixLib.MathTraits.CallerNumbers {
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public StructNumberBase<T> CallAddition(StructNumberBase<T> left, StructNumberBase<T> right) {
+#if NET7_0_OR_GREATER
+            return left.Value + right.Value;
+#else
             return TraitsINumberBaseV2<T>.Instance.Addition(left.Value, right.Value);
+#endif // NET7_0_OR_GREATER
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public StructNumberBase<T> CallMultiply(StructNumberBase<T> left, StructNumberBase<T> right) {
+#if NET7_0_OR_GREATER
+            return left.Value * right.Value;
+#else
             return TraitsINumberBaseV2<T>.Instance.Multiply(left.Value, right.Value);
+#endif // NET7_0_OR_GREATER
         }
     }
 }
